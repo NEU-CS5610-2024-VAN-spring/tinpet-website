6007ab67e7ad4c18cbe0797354cccbd59a818405
Shirui Chen <chen.shir@northeastern.edu>
2024-04-25 01:14:34 -0700

    Feat: modification in navigation bar

--- tinpet-frontend/src/components/NavigationBar.jsx
+++ tinpet-frontend/src/components/NavigationBar.jsx
@@ -9,19 +9,17 @@ function NavigationBar() {
   return (
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center">
-          <button
-            className="text-white inline-block md:hidden"
-            onClick={() => setIsMenuOpen(!isMenuOpen)}
-          >
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" /></svg>
-          </button>
-        </div>
-        <ul className={`flex-col md:flex-row md:flex space-x-0 md:space-x-6 mt-4 md:mt-0 ${isMenuOpen ? 'flex' : 'hidden'} md:flex`}>
+        <button
+          className="text-white inline-block md:hidden"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" /></svg>
+        </button>
+        <ul className={`md:flex flex-col md:flex-row items-center ${isMenuOpen ? 'flex' : 'hidden'} space-y-2 md:space-y-0 md:space-x-6 mt-4 md:mt-0`}>
           <li>
             <Link
               to="/"
-              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400 mt-2 md:mt-0"
+              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400"
             >
               Home
             </Link>
@@ -29,7 +27,7 @@ function NavigationBar() {
           <li>
             <Link
               to="/details"
-              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400 mt-2 md:mt-0"
+              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400"
             >
               Details
             </Link>
@@ -37,7 +35,7 @@ function NavigationBar() {
           <li>
             <Link
               to="/profile"
-              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400 mt-2 md:mt-0"
+              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400"
             >
               Profile
             </Link>
@@ -45,7 +43,7 @@ function NavigationBar() {
           <li>
             <Link
               to="/matches"
-              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400 mt-2 md:mt-0"
+              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400"
             >
               Matches
             </Link>
@@ -53,7 +51,7 @@ function NavigationBar() {
           <li>
             <Link
               to="/authdebugger"
-              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400 mt-2 md:mt-0"
+              className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-gray-400"
             >
               AuthDebugger
             </Link>
@@ -61,8 +59,8 @@ function NavigationBar() {
           {isAuthenticated ? (
             <li>
               <button
-                onClick={() => logout({ returnTo: "https://tinpet-phi.vercel.app/" })}
-                className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:text-gray-400 mt-2 md:mt-0"
+                onClick={() => logout({ returnTo: "https://tinpet-phi.vercel.app/"})}
+                className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:text-gray-400"
               >
                 Logout
               </button>
@@ -71,7 +69,7 @@ function NavigationBar() {
             <li>
               <button
                 onClick={loginWithRedirect}
-                className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:text-gray-400 mt-2 md:mt-0"
+                className="block bg-blue-500 text-white px-4 py-2 rounded-md hover:text-gray-400"
               >
                 Login
               </button>
